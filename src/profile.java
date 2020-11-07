cc5f2afb5dbb1122f029f59a2f242fcc80e9db1a
Simon <simon-glomb@rwth-aachen.de>
2020-11-07 10:55:13 +0100

    profile

--- src/profile.java
+++ src/profile.java
@@ -9,16 +9,32 @@ public class Profile {
   {
 	  this.username = username;
 	  this.password = password;
+
+	  
+
   }
 
   public boolean checkPassword()
   {
-	  for (i = 1; i <= 6; i++){
+	  for (int i = 1; i <= 6; i++){
 		  if((username.equals(accounts[i][0])) && (password.equals(accounts[i][1])))
 	    	  return true;
 	    	}
 	    	
 	    	return false;
-	    }
+		}
+
+	public String[][] updateAccounts(Profile newProfile, String[][] accounts){
+
+	String[][] new_accounts = new String[accounts.length+1][2];
+	for(int i=0; i<accounts.length; i++){
+		new_accounts[i][0] = accounts[i][0];
+		new_accounts[i][1] = accounts[i][1];			
+	}
+	new_accounts[accounts.length][0] = newProfile.username;
+	new_accounts[accounts.length][1] = newProfile.password;
+
+	}
+	
 
 }
