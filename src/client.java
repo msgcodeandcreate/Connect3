6b0ea7dbc7325807d3acfb7a3e6e7134e495d6dc
simonmichau <57032947+simonmichau@users.noreply.github.com>
2020-11-07 09:48:50 +0100

    Update

--- src/client.java
+++ src/client.java
@@ -7,7 +7,13 @@ public class client {
         int port = 4444;
         try {
             clientSocket = new Socket("localhost", port);
+            Thread.sleep(1000);
+            Thread server = new Thread(new serverThread(clientSocket));
+            server.start();
         } catch (IOException e){
+            System.err.println("Connection Failed.");
+            e.printStackTrace();
+        } catch (InterruptedException e){
             System.err.println("Connection Failed.");
             e.printStackTrace();
         }
--- src/serverThread.java
+++ src/serverThread.java
@@ -1,2 +1,19 @@
-public class serverThread {
+import java.awt.image.BufferedImage;
+import java.net.Socket;
+
+public class serverThread implements Runnable{
+
+    private Socket socket;
+    private String name;
+    private BufferedReader serverIn;
+    private BufferedImage clientIn;
+
+    public serverThread(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        //TODO make thread relay messages
+    }
 }
