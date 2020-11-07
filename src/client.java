bf5a4e16730c733c7d2761b9b89b5e046b5f546b
simonmichau <57032947+simonmichau@users.noreply.github.com>
2020-11-07 10:06:58 +0100

    Completed Client

--- src/client.java
+++ src/client.java
@@ -1,14 +1,19 @@
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class client {
     public static void main(String[] args){
         Socket clientSocket = null;
+        System.out.println("Please enter username: ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        scan.close();
         int port = 4444;
         try {
             clientSocket = new Socket("localhost", port);
             Thread.sleep(1000);
-            Thread server = new Thread(new serverThread(clientSocket));
+            Thread server = new Thread(new serverThread(clientSocket, name));
             server.start();
         } catch (IOException e){
             System.err.println("Connection Failed.");
--- src/serverThread.java
+++ src/serverThread.java
@@ -1,6 +1,7 @@
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -20,9 +21,23 @@ public class serverThread implements Runnable{
     @Override
     public void run() {
         try {
-            out = new PrintWriter(socket)
+            out = new PrintWriter(socket.getOutputStream(), true);
+            serverIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            clientIn = new BufferedReader(new InputStreamReader(System.in));
+
+            while (!socket.isClosed()){
+                if (serverIn.ready()){
+                    String input = serverIn.readLine();
+                    if (input != null){
+                        System.out.println(input);
+                    }
+                }
+                if (clientIn.ready()){
+                    out.println(name + ": " + clientIn.readLine());
+                }
+            }
         } catch (IOException e){
-            e.printStackTrace();;
+            e.printStackTrace();
         }
     }
 }
