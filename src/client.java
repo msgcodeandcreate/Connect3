4e37cbc6443428559b2d331847e11d54e765bd8d
simonmichau <57032947+simonmichau@users.noreply.github.com>
2020-11-07 09:36:53 +0100

    Update

--- src/client.java
+++ src/client.java
@@ -1,2 +1,15 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class client {
+    public static void main(String[] args){
+        Socket clientSocket = null;
+        int port = 4444;
+        try {
+            clientSocket = new Socket("localhost", port);
+        } catch (IOException e){
+            System.err.println("Connection Failed.");
+            e.printStackTrace();
+        }
+    }
 }
