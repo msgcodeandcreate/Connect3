35311f7a1aa033f22385162036035577957aa3bc
Simon <simon-glomb@rwth-aachen.de>
2020-11-07 10:28:33 +0100

    clientThread

--- src/clientThread.java
+++ src/clientThread.java
@@ -1,7 +1,30 @@
 import java.net.*;
 import java.io.*;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
-public class clientThread {
-    ClientThread clients = new ArrayList<ClientThread>();
+public class clientThread implements Runnable {
+    ArrayList<clientThread> clients = new ArrayList<clientThread>();
+
+    private String name;
+    private String password;
+    private Socket socket;
+    private PrintWriter out;
+
+    public clientThread(String name, String password, Socket socket){
+        this.name = name;
+        this.password = password;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        try{
+            out = new PrintWriter(socket.getOutputStream());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 
 }
