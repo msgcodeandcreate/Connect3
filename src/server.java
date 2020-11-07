c66c773eaf4b2d04aaf0cc1416bec8a1465c8c9c
Simon <simon-glomb@rwth-aachen.de>
2020-11-07 09:54:54 +0100

    Server

--- src/server.java
+++ src/server.java
@@ -1,2 +1,31 @@
+import java.net.*;
+import java.io.*;
+
 public class server {
+    //private ServerSocket serverSocket;
+    public static void main(String[] args){
+        ServerSocket serverSocket;
+        int portNumber =4444;
+        //initialisiere den Socket
+        try {
+            serverSocket = new ServerSocket(portNumber);
+
+        } catch (IOException e){
+            System.err.println("Port: "+portNumber+" konnte nicht benutzt werden");
+            System.exit(1);
+        }
+
+        //akzeptiere alle Clients
+        while(true){
+            try{
+                
+				Socket socket = serverSocket.accept();
+            }catch(IOException e){
+                System.out.println("Accept failed on: "+portNumber)
+            }
+        }
+
+    }
+
+
 }
